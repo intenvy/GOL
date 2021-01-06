1c6a5ad6b1e686dfddb7997db935a788efd11716
Parsa Saberi <parsa.xmule@gmail.com>
2021-01-06 19:39:16 +0330

    implement game itself

--- gol/gol.py
+++ gol/gol.py
@@ -1,4 +1,9 @@
 from abc import ABC, abstractmethod
+from typing import List
+from time import sleep as time_sleep
+import numpy as np
+import os
+from gol.config import GolConfig
 
 
 class FrameSimulation(ABC):
@@ -7,10 +12,13 @@ class FrameSimulation(ABC):
     def _next_frame(self) -> None:
         pass
 
-    @abstractmethod
     def step(self, size: int) -> None:
         for i in range(size):
             self._next_frame()
+
+    @abstractmethod
+    def run(self, frames: int) -> None:
+        pass
 
     @abstractmethod
     def display(self) -> None:
@@ -18,15 +26,43 @@ class FrameSimulation(ABC):
 
 
 class GameOfLife(FrameSimulation):
+    __slots__ = [
+        'world', 'rows', 'cols', 'char_map', 'cast',
+        'frame_sleep', 'skip_frames',
+    ]
 
-    __slots__ = 'world', 'dead_cell_character', 'alive_cell_character', 'frame_sleep', 'skip_frames'
+    def __init__(self, world: np.ndarray, config: GolConfig):
+        self.world = world
+        self.rows = world.shape[0]
+        self.cols = world.shape[1]
+        self.char_map: List[str] = [config.display_dead_cell, config.display_alive_cell]
+        self.cast = np.vectorize(lambda cell: self.char_map[cell])
+        self.frame_sleep = config.frame_sleep
+        self.skip_frames = config.skip_frames
+
+    def _compute_neighbours(self) -> np.ndarray:
+        N = np.zeros(np.shape(self.world))
+        N = np.roll(self.world, 1, axis=1) + \
+            np.roll(self.world, -1, axis=1) + \
+            np.roll(self.world, 1, axis=0) + \
+            np.roll(self.world, -1, axis=0)
+        return N
 
     def _next_frame(self) -> None:
-        pass
-
-    def step(self, size: int) -> None:
-        pass
+        N = self._compute_neighbours()
+        zeros = (self.world == 1) & ((N < 2) | (N > 3))
+        ones = (self.world != 1) & (N == 3)
+        self.world[zeros] = 0
+        self.world[ones] = 1
 
     def display(self) -> None:
-        pass
+        _ = os.system('cls') if os.name.startswith('nt') else os.system('clear')
+        out = '\n'.join(''.join(self.cast(row)) for row in self.world)
+        print(out)
 
+    def run(self, frames: int) -> None:
+        for frame in range(frames):
+            print('s')
+            self.display()
+            self.step(self.skip_frames+1)
+            time_sleep(self.frame_sleep)
