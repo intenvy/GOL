3ce798c02a61fea8ab225767bf467ba0e4df4cbd
Parsa Saberi <parsa.xmule@gmail.com>
2021-01-06 19:39:24 +0330

    implement map_reader

--- gol/map_reader.py
+++ gol/map_reader.py
@@ -1,11 +1,20 @@
-from .config import GolConfig
-from numpy import ndarray
+from typing import List, Dict
+from numpy import array
+from gol.config import GolConfig
 
-class MapReader(object):
 
-    __slots__ = 'base_directory',
+class MapReader:
 
-    def __int__(self, directory: str, Gol):
-        self.base_directory = directory
+    __slots__ = 'base_directory', 'dead_cell', 'alive_cell'
 
-    def read
+    def __init__(self, config: GolConfig) -> None:
+        self.base_directory = config.map_directory
+        self.dead_cell = config.reader_dead_cell
+        self.alive_cell = config.reader_alive_cell
+
+    def read(self, map_file: str) -> array:
+        with open(f'{self.base_directory}{map_file}') as file:
+            content: List[str] = file.read().strip().split('\n')
+            _value_map: Dict[str, int] = {self.dead_cell: 0, self.alive_cell: 1}
+            mapped_array = [list(map(lambda char: _value_map[char], list(line))) for line in content]
+            return array(mapped_array)
